e6070e26ccd46e7213df6f86a6182426a5b8d147
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-22 18:30:53 +0700

    update bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-row justify-between items-center w-full h-16 px-4 fixed z-10 bg-zinc-950 text-white border-[0.5px] border-gray-300 border-opacity-30">
+    class="flex flex-row justify-between items-center w-full h-16 px-4 fixed z-10 bg-zinc-950 text-white border-[0.5px] border-gray-500 border-opacity-50">
     <div class="flex items-center gap-6 ml-3">
       <div class="cursor-pointer w-fit" @click="menuClick">
         <div class="w-[18px] h-[1.1px] bg-white my-[5px]"></div>
@@ -12,7 +12,7 @@
         <img src="../assets/youtube_music.png" class="w-28" />
       </a>
 
-      <div class="flex w-full min-w-[420px] bg-zinc-800 rounded-lg border-[0.5px] border-gray-300 border-opacity-30">
+      <div class="flex w-full min-w-[420px] bg-zinc-800 rounded-lg border-[0.5px] border-gray-500 border-opacity-50">
         <img src="../assets/search.svg" class="w-5 mx-4"/>
         <input type="text"
           class="w-full mr-6 bg-zinc-800 text-[#9e9e9e] font-light p-2 focus:outline-none placeholder-current"
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="flex flex-col h-screen w-20 bg-zinc-950 text-white fixed py-20">
+  <div class="flex flex-col h-screen w-20 bg-zinc-950 text-white fixed py-20 items-center text-center border-[0.5px] border-gray-500 border-opacity-50">
     <ul>
-      <li v-for="(menuItem, index) in menuItems" :key="index">
-        <img :src="menuItem.image" alt="Menu Icon" class="w-8 h-8 my-2" />
-        {{ menuItem.label }}
+      <li class="text-xs mb-6">
+        <img src="../assets/home.svg" :alt="menuItems[0].label" class="w-5 h-5 m-2" />
+        {{ menuItems[0].label }}
+      </li>
+      <li class="text-xs mb-6">
+        <img src="../assets/explore.svg" :alt="menuItems[1].label" class="w-5 h-5 m-2" />
+        {{ menuItems[1].label }}
+      </li>
+      <li class="text-xs mb-6">
+        <img src="../assets/library.svg" :alt="menuItems[2].label" class="w-5 h-5 m-2" />
+        {{ menuItems[2].label }}
       </li>
     </ul>
   </div>
@@ -17,6 +25,4 @@ const menuItems = [
 ];
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
