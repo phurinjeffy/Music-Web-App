b463d81496723de0d8100601b43d544e694dc56c
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-22 18:35:38 +0700

    update

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="flex flex-col h-screen w-20 bg-zinc-950 text-white fixed py-20 items-center text-center border-[0.5px] border-gray-500 border-opacity-50">
     <ul>
-      <li class="text-xs mb-6">
-        <img src="../assets/home.svg" :alt="menuItems[0].label" class="w-5 h-5 m-2" />
-        {{ menuItems[0].label }}
-      </li>
-      <li class="text-xs mb-6">
-        <img src="../assets/explore.svg" :alt="menuItems[1].label" class="w-5 h-5 m-2" />
-        {{ menuItems[1].label }}
-      </li>
-      <li class="text-xs mb-6">
-        <img src="../assets/library.svg" :alt="menuItems[2].label" class="w-5 h-5 m-2" />
-        {{ menuItems[2].label }}
+      <li v-for="(menuItem, index) in menuItems" :key="index" class="text-xs mb-6">
+        <img :src="menuItem.image" :alt="menuItem.label" class="w-5 h-5 m-2" />
+        {{ menuItem.label }}
       </li>
     </ul>
   </div>
@@ -19,9 +11,9 @@
 
 <script setup lang="ts">
 const menuItems = [
-  { label: 'Home', image: '../assets/home.svg' },
-  { label: 'Explore', image: '../assets/explore.svg' },
-  { label: 'Library', image: '../assets/library.svg' },
+  { label: 'Home', image: '/src/assets/home.svg' },
+  { label: 'Explore', image: '/src/assets/explore.svg' },
+  { label: 'Library', image: '/src/assets/library.svg' },
 ];
 </script>
 
