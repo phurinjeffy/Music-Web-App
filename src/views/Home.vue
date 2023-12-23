100fdcaba32b0404b61c3c72196f3c24dd34b6c0
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-23 21:52:38 +0700

    swap home and library

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,13 @@
 <template>
   <main>
-    <ul class="flex flex-col justify-center items-start my-12" ref="musicList">
-      <li v-for="(music, index) in getMusics" :key="index" :class="checkActiveMusic(music.index) ? 'bg-zinc-900' : ''"
-        @click="selectMusic(music); updateMusicListScroll($event);" :ref="checkActiveMusic(music.index) ? 'activeMusic' : ''"
-        class="flex flex-row w-fit items-center justify-center mx-auto p-3">
-        <img :src="music.cover" class="w-16 h-16 mr-5" />
-        <span class="w-96 text-white">{{ music.name }}</span>
-        <span class="w-96 text-zinc-400">{{ music.artist }}</span>
-        <span class="w-96 text-zinc-400">{{ music.album }}</span>
-      </li>
-    </ul>
+
   </main>
 </template>
 
-<script lang="js">
-import globalMixins from "../mixins/globalMixins";
-import musicsMixins from "../mixins/musicsMixins";
-export default {
-  name: "Home",
-  mixins: [globalMixins, musicsMixins],
-};
+<script setup lang="ts">
+
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
