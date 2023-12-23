fb841b7fbfed81d2a9b1dec559ef4f3fd913c4dd
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-23 21:19:43 +0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,11 @@
     <ul class="flex flex-col justify-center items-start my-12" ref="musicList">
       <li v-for="(music, index) in getMusics" :key="index" :class="checkActiveMusic(music.index) ? 'bg-zinc-900' : ''"
         @click="selectMusic(music); updateMusicListScroll($event);" :ref="checkActiveMusic(music.index) ? 'activeMusic' : ''"
-        class="flex flex-row w-fit my-2 items-center justify-center mx-auto p-2">
+        class="flex flex-row w-fit items-center justify-center mx-auto p-3">
         <img :src="music.cover" class="w-16 h-16 mr-5" />
-        <span class="w-96">{{ music.name }}</span>
-        <span class="w-96">{{ music.artist }}</span>
-        <span class="w-96">{{ music.album }}</span>
+        <span class="w-96 text-white">{{ music.name }}</span>
+        <span class="w-96 text-zinc-400">{{ music.artist }}</span>
+        <span class="w-96 text-zinc-400">{{ music.album }}</span>
       </li>
     </ul>
   </main>
