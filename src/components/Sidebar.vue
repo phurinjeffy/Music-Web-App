1e5ffcb36645e98d8ef996fef4323c1d74c13094
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-23 09:01:29 +0700

    add constants (MENU_ITEMS)

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,24 +1,14 @@
 <template>
   <div class="flex flex-col h-screen w-20 bg-zinc-950 text-white fixed py-20 items-center text-center border-[0.5px] border-gray-500 border-opacity-50">
-    <router-link to="/" class="text-xs mb-6 cursor-pointer">
-      <img src="/src/assets/home.svg" alt="Home" class="w-5 h-5 m-2" />
-      Home
-    </router-link>
-
-    <router-link to="/explore" class="text-xs mb-6 cursor-pointer">
-      <img src="/src/assets/explore.svg" alt="Explore" class="w-5 h-5 m-2" />
-      Explore
-    </router-link>
-
-    <router-link to="/library" class="text-xs mb-6 cursor-pointer">
-      <img src="/src/assets/library.svg" alt="Library" class="w-5 h-5 m-2" />
-      Library
+    <router-link v-for="menuItem in MENU_ITEMS" :key="menuItem.label" :to="menuItem.route" class="text-xs mb-6 cursor-pointer">
+      <img :src="menuItem.image" :alt="menuItem.label" class="w-5 h-5 m-2" />
+      {{ menuItem.label }}
     </router-link>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { MENU_ITEMS } from '../constants';
 </script>
 
 <style scoped></style>
---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const MENU_ITEMS = [
+  { label: 'Home', route: '/', image: '/src/assets/home.svg'},
+  { label: 'Explore', route: '/explore', image: '/src/assets/explore.svg' },
+  { label: 'Library', route: '/library', image: '/src/assets/library.svg' },
+];
