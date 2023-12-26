a5c0a5da351432f4515130b7b191f51b2ff699f2
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-26 13:57:17 +0700

    update music player styles

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="bottom-0 flex flex-row justify-between items-center w-full h-20 px-4 fixed z-10 bg-zinc-800 text-white border-[0.5px] border-gray-500 border-opacity-50">
-    <div class="flex items-center gap-8 ml-1">
+    <div class="flex items-center gap-5 md:gap-8 ml-2">
       <button>
         <img src="../assets/prev.svg" class="w-4" />
       </button>
@@ -13,23 +13,23 @@
       </button>
     </div>
 
-    <div v-if="currentTrack" class="flex w-fit min-w-[420px] gap-4 items-center">
-      <div class="w-11 h-11 rounded-sm overflow-hidden">
+    <div v-if="currentTrack" class="flex w-fit g:min-w-[420px] gap-4 items-center mx-4">
+      <div class="w-11 h-11 rounded-sm hidden md:block">
         <img :src="currentTrack.cover" :alt="currentTrack.albumName" class="w-full h-full object-cover" />
       </div>
 
-      <div class="flex flex-col justify-center flex-grow">
-        <a href="" class="text-base font-normal w-fit hover:underline">{{ currentTrack.name }}</a>
-        <p class="text-base font-light text-zinc-400">
+      <div class="flex flex-col justify-center">
+        <a href="" class="text-sm md:text-base font-normal w-fit hover:underline">{{ currentTrack.name }}</a>
+        <p class="text-sm md:text-base font-light text-zinc-400 truncate">
           <span><a href="" class="hover:underline">{{ currentTrack.album }}</a></span> •
           <span><a href="" class="hover:underline">{{ currentTrack.artist }}</a></span>
         </p>
       </div>
     </div>
 
-    <div class="mr-6 flex items-center">
-      <input type="range" class="mr-4 accent-white w-24 h-1 cursor-pointer" min="0" max="100" v-model="currentVolume"
-        @input="updateVolume" />
+    <div class="mr-6 flex items-center group" @mouseover="showVolume = true" @mouseleave="showVolume = false">
+      <input type="range" class="mr-4 accent-white w-24 h-1 cursor-pointer hidden lg:block group-hover:block" min="0"
+        max="100" v-model="currentVolume" @input="updateVolume" />
       <button @click="toggleVolume">
         <img :src="isMute ? 'src/assets/mute.svg' : 'src/assets/volume.svg'" class="w-6" />
       </button>
@@ -46,6 +46,7 @@ import { ref, computed, onMounted } from 'vue';
 const isPlaying = ref(false);
 const isMute = ref(false);
 const volume = ref(100);
+const showVolume = ref(false);
 
 const store = useStore();
 
