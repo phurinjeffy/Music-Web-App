ffc39bed8b3d0116d2a9fcf5149ac7310446c089
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-27 10:20:55 +0700

    re-organize directories

---
+++ client/src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+// const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=eecd535d69bc42339419202f5f851fdd&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+</script>
+
+<style scoped>
+</style>
