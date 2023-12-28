a513e0d35c47687de9c3195817f1ff3fcc8cc4aa
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-28 22:32:45 +0700

    update styles

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,15 +1,16 @@
 <template>
   <div
     class="flex flex-row justify-between items-center w-full h-16 px-4 fixed z-10 bg-zinc-950 text-white border-[0.5px] border-gray-500 border-opacity-50">
-    <div class="flex items-center gap-6 ml-3">
+    <div class="flex items-center gap-8 ml-3">
       <div class="cursor-pointer w-fit" @click="toggleMenu">
         <div class="w-[18px] h-[1.1px] bg-white my-[5px]"></div>
         <div class="w-[18px] h-[1.1px] bg-white my-[5px]"></div>
         <div class="w-[18px] h-[1.1px] bg-white my-[5px]"></div>
       </div>
 
-      <router-link to="/">
+      <router-link to="/" class="flex flex-row items-center gap-4">
         <img src="../assets/youtube_music.png" class="w-24" />
+        <!-- <p class="font-signature font-semibold text-4xl mb-2">jeff</p> -->
       </router-link>
     </div>
 
