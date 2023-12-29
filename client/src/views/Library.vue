3524f5ff709b2e2c968000f6c13d940bb6de1528
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2023-12-29 08:04:31 +0700

    update

--- client/src/views/Library.vue
+++ client/src/views/Library.vue
@@ -1,39 +1,29 @@
 <template>
-  <div class="mx-6">
-    <ul class="flex flex-col justify-center items-start my-12" ref="musicList">
-      <li v-for="(music, index) in getMusics" :key="index"
-        class="flex flex-row w-full items-center justify-center mx-auto p-3"
-        @click="setActiveMusic(music)"
-        :class="{ 'bg-zinc-900': isActiveTrack(music) }"
-        >
+  <div class="p-8">
+    <div class="flex flex-row gap-5 ml-2 items-center">
+      <button type="button" class="text-white text-xl font-semibold hover:underline cursor-pointer">
+        Playlists
+      </button>
+      <button type="button" class="text-white text-xl font-semibold hover:underline cursor-pointer">
+        Songs
+      </button>
+      <button type="button" class="text-white text-xl font-semibold hover:underline cursor-pointer">
+        Albums
+      </button>
+      <button type="button" class="text-white text-xl font-semibold hover:underline cursor-pointer">
+        Artists
+      </button>
+    </div>
 
-        <img :src="music.cover" class="w-16 h-16 mr-5" />
-        <span class="w-96 text-white">{{ music.name }}</span>
-        <span class="w-96 text-zinc-400">{{ music.artist }}</span>
-        <span class="w-96 text-zinc-400">{{ music.album }}</span>
+    <div class="py-1.5"></div>
 
-      </li>
-    </ul>
+    <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useStore } from 'vuex';
-import { Music } from 'src/store';
-
-const store = useStore();
-
-const getMusics = store.getters.getMusics;
-
-const setActiveMusic = (music: Music) => {
-  store.commit('setActiveMusic', music);
-  store.commit('setAudioSource', music.file);
-};
-
-const isActiveTrack = (music: Music) => {
-  const activeMusic = store.getters.getActiveMusic;
-  return activeMusic.index === music.index;
-};
 
 </script>
 
---
+++ client/src/views/Playlist.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { useStore } from 'vuex';
+import { Music } from 'src/store';
+
+const store = useStore();
+
+const getMusics = store.getters.getMusics;
+
+const setActiveMusic = (music: Music) => {
+  store.commit('setActiveMusic', music);
+  store.commit('setAudioSource', music.file);
+};
+
+const isActiveTrack = (music: Music) => {
+  const activeMusic = store.getters.getActiveMusic;
+  return activeMusic.index === music.index;
+};
+</script>
+
+<template>
+  <div class="mx-6">
+    <ul class="flex flex-col justify-center items-start my-12" ref="musicList">
+      <li v-for="(music, index) in getMusics" :key="index"
+        class="flex flex-row w-full items-center justify-center mx-auto p-3"
+        @click="setActiveMusic(music)"
+        :class="{ 'bg-zinc-900': isActiveTrack(music) }"
+        >
+
+        <img :src="music.cover" class="w-16 h-16 mr-5" />
+        <span class="w-96 text-white">{{ music.name }}</span>
+        <span class="w-96 text-zinc-400">{{ music.artist }}</span>
+        <span class="w-96 text-zinc-400">{{ music.album }}</span>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
